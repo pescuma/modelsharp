3cc50929d38e816404d51d9c6a8e0789afbd30c8
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2010-11-02 21:11:24 +0000

    Added using Renamed package to namespace

---
+++ examples/build.bat
@@ -0,0 +1,9 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86
+
+java -jar trang.jar "simple\model.xml" "immutable\model.xml" "composition\model.xml" "collection\model.xml" "lazy\model.xml" "using\model.xml" model_xml.xsd
+
+xsd model_xml.xsd /classes /n:org.pescuma.ModelSharp.xml /out:..\xml
+
+pause
